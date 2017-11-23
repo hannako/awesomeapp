7645930210a4013ac735009838ceca6e908c4ce2
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2017-11-23 13:52:17 +0000

    add travis

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_animal
 
   # GET /posts
   # GET /posts.json
@@ -70,5 +71,10 @@ class PostsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:name, :title, :content)
+    end
+
+    def set_animal
+      services = JSON.parse(ENV["VCAP_SERVICES"])
+      @animal = services["user-provided"][0]["credentials"]["animal"]
     end
 end
--- config/initializers/secrets.rb
+++
@@ -1,3 +0,0 @@
-return unless ENV["VCAP_SERVICES"]
-
-animal = sys_env["VCAP_SERVICES"]["user-provided"]["animal"]
